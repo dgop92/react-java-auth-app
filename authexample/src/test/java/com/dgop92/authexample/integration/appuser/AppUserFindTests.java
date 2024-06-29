8cd43acb52685f5407ad096c36e95f7fc8add5e9
dgop92 <inevaup@gmail.com>
2024-06-29 09:26:25 -0500

    fix(rest-api): remove AutoConfigureTestDatabase annotation from persistence tests

--- authexample/src/test/java/com/dgop92/authexample/integration/appuser/AppUserFindTests.java
+++ authexample/src/test/java/com/dgop92/authexample/integration/appuser/AppUserFindTests.java
@@ -13,12 +13,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class AppUserFindTests {
 
     @Autowired
--- authexample/src/test/java/com/dgop92/authexample/integration/user/UserIdpPersistenceTests.java
+++ authexample/src/test/java/com/dgop92/authexample/integration/user/UserIdpPersistenceTests.java
@@ -15,15 +15,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 
 @SpringBootTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserIdpPersistenceTests {
 
     @Autowired
