07e79baf3678bc9266b3c4ae1a3c399d1a87ae7c
Diego <45317885+dgop92@users.noreply.github.com>
2024-07-28 17:56:02 -0500

    feat: add comment for main app

--- authexample/src/main/java/com/dgop92/authexample/AuthexampleApplication.java
+++ authexample/src/main/java/com/dgop92/authexample/AuthexampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthexampleApplication {
 
 	public static void main(String[] args) {
+		// Dummy change
 		SpringApplication.run(AuthexampleApplication.class, args);
 	}
 
